eadf9b9585fd07933890c7dd40706219970b44b1
Arham-Bhabha <99017594+Arham-Bhabha@users.noreply.github.com>
2022-06-15 14:42:57 +0530

    Add files via upload

---
+++ room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCktZOF9ESddNA-lQZwUUhNoux_zR2fpSg",
+    authDomain: "kwitter-prac-9e7f6.firebaseapp.com",
+    databaseURL: "https://kwitter-prac-9e7f6-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kwitter-prac-9e7f6",
+    storageBucket: "kwitter-prac-9e7f6.appspot.com",
+    messagingSenderId: "795145209128",
+    appId: "1:795145209128:web:660d3cb33b57b331c3fc1d"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
